c836b38793fc21ebf315a09129c5eb9187491896
Zaden Connell <zaden.connell@gmail.com>
2024-01-29 09:34:11 -0500

    add: GUID test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,17 +1,12 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
+import { generateGUID } from '../../guidGenerator';
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
 
-    /* test('generateGUID generates a valid GUID', () => {
+    test('generateGUID generates a valid GUID', () => {
         const guid = generateGUID();
         assert.match(guid, /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
     });
-
-    test('generateVSProjectFiles generates the correct files', async () => {
-        // TODO: Set up the necessary preconditions for the test
-        await generateVSProjectFiles();
-        // TODO: Check that the correct files were generated
-    }); */
 });
