fc4c6ade8688a5675495a05cfc70ff4d522a6ae5
Zaden Connell <zaden.connell@gmail.com>
2024-02-05 17:04:11 -0500

    Update GUID format in generateGUID function

--- src/guidGenerator.ts
+++ src/guidGenerator.ts
@@ -1,5 +1,5 @@
 export function generateGUID() {
-    return 'xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[x]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         const r = Math.random() * 16 | 0;
         const v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
